65f09761b5d49a4a244041456dec3d248b3cb237
Abhijit Jain <abhijitjain.aj@gmail.com>
2015-05-26 13:05:08 +0530

    Re Pythonic Urbandict-in

--- urbandict.py
+++ urbandict.py
@@ -1,37 +1,29 @@
 from bs4 import BeautifulSoup as BS
 import requests,sys
 
-def reveal(query):
+def reveal():
 
     '''Finds the query result from the Urban Dict website.'''
     
-    ''' Tweak this if you want to use the script from the command line.
-
     if len(sys.argv) > 1:
         query = " ".join(sys.argv[1:])
         query = query.replace(" ","+")
     else:
-        query = " "
-        raise ValueError("You provided me with nothing!")
-    '''
+        query = str(input("What do you wanna look up? "))
+    
     base_url = "http://www.urbandictionary.com/define.php?term="
     new_url = base_url + query
 
     result = requests.get(new_url)
     resultSoup = BS(result.text)
     meaning = resultSoup.select('.meaning')
-
-    count = 0
+    
     if len(meaning) > 0:
-        for each in meaning:
+        for each in meaning[:3]: #Top three results.
             print(each.getText())
-            count += 1
-            if count == 3: #Currently shows top three results. Change this if you have to.
-                break
     else:
         print("\n..Oops. What you looked up does not exist!")
 
 if __name__ == "__main__":
     
-    key = str(input("What do you wanna look up? "))
-    reveal(key)
+    reveal()
